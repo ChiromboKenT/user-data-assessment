d706f646a99890aa7185bb2e708fcef287e6207b
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2022-09-01 10:23:57 +0200

    Update css reset defaults

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Card from "./components/standard-elements/Card";
 function App() {
   return (
     <>
-      <Card heading="User Data"></Card>
+      <Card heading="User Data" height="100%" width="100%" m="0"></Card>
     </>
   );
 }
